3ecd62bf6d122de6cb59d2cc506832654690b66b
george-koshy1 <george.koshy1@allianz.com>
2019-01-17 16:04:32 +0000

    added health check endpoint

--- src/main/java/com/aztech/demo/controller/BatchController.java
+++ src/main/java/com/aztech/demo/controller/BatchController.java
@@ -9,17 +9,22 @@ import java.util.Optional;
 
 
 @RestController
-@RequestMapping(value = "/batch")
+@RequestMapping(value = "/")
 public class BatchController {
 
     @Autowired
     private DemoService demoService;
 
 
-@GetMapping("{batchid}/status")
+@GetMapping("batch/{batchid}/status")
     public String getBatchDetails(@PathVariable("batchid") Integer batchId) {
         Optional<BatchDetails> batchDetails = demoService.getBatchDetails(batchId);
         return "<b>" + batchDetails.get().getStatus() + "</b>";
+    }
+
+@GetMapping("healthcheck")
+    public String getBatchDetails() {
+        return "<b>The container is healthy!!</b>";
     }
 
 }
